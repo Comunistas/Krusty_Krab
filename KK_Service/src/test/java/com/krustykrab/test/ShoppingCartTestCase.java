4d08d9607f85edd6ecfde8b6e4fcef1c2db79927
Vladislav Elias Zedano Arjipova <vlzedano@Mac-mini-de-VZEDANO.local>
2016-12-09 13:47:47 -0500

    Minor changes.

--- KK_Service/src/test/java/com/krustykrab/test/ShoppingCartTestCase.java
+++ KK_Service/src/test/java/com/krustykrab/test/ShoppingCartTestCase.java
@@ -1,7 +1,12 @@
 package com.krustykrab.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Collection;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -18,13 +23,17 @@ import com.krustykrab.service.impl.KrustyKrabOrderServiceImpl;
 import com.krustykrab.utils.ShoppingCartUtil;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties={"spring.jpa.properties.hibernate.hbm2ddl.import_files=mock-data.sql"})
+@SpringBootTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ShoppingCartTestCase {
 
 	@Autowired ShoppingCartUtil shoppingCartUtil;
 	@Autowired KrustyKrabDishServiceImpl dishService;
 	@Autowired KrustyKrabOrderServiceImpl orderService;
+	
+	long tableId = 1L;
+	long firstDishId = 1L;
+	long secondDishId = 2L;
 	
 	/*
 	@BeforeClass
@@ -37,43 +46,100 @@ public class ShoppingCartTestCase {
 		assertTrue(shoppingCartUtil.createANewOrder(1L, 1L));
 	}
 	
+	@After
+	public void destroyCart(){
+		shoppingCartUtil.deleteOrder(1L);
+	}
+	
+	private void addTwoCarts() {
+		Dish dish1 = dishService.getEntity(firstDishId);
+		Dish dish2 = dishService.getEntity(secondDishId);
+		
+		shoppingCartUtil.addDishesToOrder(tableId, dish1, 5);
+		shoppingCartUtil.addDishesToOrder(tableId, dish2, 2);
+	}
+	
 	
 	/**
-	 * This test is intended to test if the cart is actually being created,
+	 * This test is intended to check if the cart is actually being created,
 	 * and a ShoppingCartResponse object is being created
 	 */
 	@Test
 	public void test_01_AddNewOrder() {
-		assertTrue(shoppingCartUtil.getAllCarts().stream().anyMatch(cart -> cart.getTableId()==1L));
-		assertFalse(shoppingCartUtil.getAllCarts().stream().anyMatch(cart -> cart.getTableId()==1L));
+		assertTrue(shoppingCartUtil.getAllCarts().stream().anyMatch(cart -> cart.getTableId()==tableId));
+		assertFalse(shoppingCartUtil.getAllCarts().stream().anyMatch(cart -> cart.getTableId()==2L));
 	}
 	
+	/**
+	 * This test is intended to validate that the dishes are being added succefully to the cart.
+	 */
 	@Test
 	public void test_02_AddDishesToOrder() {
 		
-		Dish dish1 = dishService.getEntity(1L);
-		Dish dish2 = dishService.getEntity(2L);
+		addTwoCarts();
 		
+		ShoppingCartResponse cart = shoppingCartUtil.getShoppingCartForTable(tableId);
 		
-		shoppingCartUtil.addDishesToOrder(1L, dish1, 3);
-		shoppingCartUtil.addDishesToOrder(1L, dish2, 1);
-		
-		ShoppingCartResponse cart = shoppingCartUtil.getShoppingCartForTable(1L);
-		
+		assertFalse(cart.getDishes().isEmpty());
+	
+		assertEquals(5, cart.getDishes().stream().filter(dishOrder -> dishOrder.getDish().getId() == firstDishId).findFirst().get().getAmount());
+		assertEquals(2,cart.getDishes().stream().filter(dishOrder -> dishOrder.getDish().getId() == secondDishId).findFirst().get().getAmount());
 		
 	}
 	
-	/*
+	
+	/**
+	 * This test is intended to check that the dishes are being removed correctly from the cart.
+	 */
 	@Test
 	public void test_03_RemoveDishesToOrder() {
-		assertTrue(shoppingCartUtil.createANewOrder(1L, 1L));
-	}/*
+		
+		addTwoCarts();
+		
+		ShoppingCartResponse cart;
+		
+		// 5 - 3 dishes
+		cart = shoppingCartUtil.removeDishesFromOrder(tableId, firstDishId, 3);
+		
+		assertEquals(2, cart.getDishes().stream().filter(dishOrder -> dishOrder.getDish().getId() == firstDishId).findFirst().get().getAmount());
+
+		// 2 - 2 dishes, the dish item should be removed
+		cart = shoppingCartUtil.removeDishesFromOrder(tableId, secondDishId, 2);
+		
+		assertFalse(cart.getDishes().stream().anyMatch(dishOrder -> dishOrder.getDish().getId() == secondDishId));
+		
+	}
+	
+	
+	
+	/**
+	 * This test checks if the entire cart is deleted for a table.
+	 */
 	@Test
 	public void test_04_DeleteCart() {
-		assertTrue(shoppingCartUtil.createANewOrder(1L, 1L));
-	}/*
+		
+		Collection<ShoppingCartResponse> carts = shoppingCartUtil.deleteOrder(tableId);
+		
+		//Since there was only one cart in the cache, the list should be empty
+		assertTrue(carts.isEmpty());
+		
+	}
+	
+	/**
+	 * 
+	 */
 	@Test
-	public void test_05_SaveCart() {
-		assertTrue(shoppingCartUtil.createANewOrder(1L, 1L));
-	}*/
+	public void test_05_RepeatedCart(){
+		
+		assertFalse(shoppingCartUtil.createANewOrder(1L, 1L));
+		assertTrue(shoppingCartUtil.createANewOrder(2L, 1L));
+		assertFalse(shoppingCartUtil.createANewOrder(2L, 1L));
+		shoppingCartUtil.deleteOrder(2L);
+	}
+	
+	
+	@Test
+	public void test_06_SaveCart() {
+		
+	}
 }
